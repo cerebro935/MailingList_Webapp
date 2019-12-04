7b5d017ee8a1de1d119ce19bd6f22aa4e0e553d9
Angel Guzman <angel.guzman935@gmail.com>
2019-12-04 13:14:46 -0800

    map updates with pins for every address

--- src/App.js
+++ src/App.js
@@ -18,10 +18,9 @@ Geocode.setRegion("us");
 
 var addressData;
 var addressString;
-var marker;
 var temp;
+var names = [];;
 var markers = [];
-var position = [39.50, -98.35];
 var mark = null;
 const [data, setData] = useState([]);
 const [geo, setGeo] = useState([]);
@@ -83,53 +82,23 @@ useEffect(() => {
     .then(result => setData(result.data));
 }, []);
 
-function addMarkers() {
- for(var index = 0; index < addressData.length; index++){
-  var addressString = (addressData[index].street).concat(' ', addressData[index].city, ' ', addressData[index].state);
-  console.log(addressString);
-  Geocode.fromAddress(addressString)
-   .then(response => {
-      markers.push(response.results[0].geometry.location);
-    },
-    error => {
-       console.error(error);
-    });
- }
-}
-
-function getCoords(address) {
-  addressString = (address.street).concat(' ', address.city, ' ', address.state);
-  Geocode.fromAddress(addressString)
-        .then(response => {
-        setGeo(response.results[0].geometry.location);
-      },
-     error => {
-         console.error(error);
-     });
-}
-
-function go() {
+useEffect(() => {
 data.map(address => (
-      marker = {lat: 39.5, lng: -98.53},
+      names.push(address.name),
       addressString = (address.street).concat(' ', address.city, ' ', address.state),
       Geocode.fromAddress(addressString)
-        .then(response => {
+      .then(response => {
         temp = response.results[0].geometry.location
         console.log(address.name)
         console.log(temp)
+        markers.push([temp.lat, temp.lng])
       },
       error => {
          console.error(error)
-      }),
-      //console.log(marker),
-      mark = (<Marker position={[marker.lat, marker.lng]}>
-        <Popup>
-          Test pop-up
-        </Popup>
-      </Marker>)
-))
-}
-
+      })
+));
+markers = [];
+});
 
   return (
     <div>
@@ -149,14 +118,19 @@ data.map(address => (
  </script>
 
    <div>
-    <button onClick={go}>Get All Addresses</button>
+    <button onClick={()=>setGeo(markers)}>Update Map</button>
     <Map center={[39.50, -98.35]} zoom={4.5}>
       <TileLayer
         attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
          />
-       <button onClick={go}>Get All Addresses</button>
-        {mark}
+        {geo.map((position, idx) =>
+           <Marker key={`marker-${idx}`} position={position}>
+            <Popup>
+              <span> {names[idx]} </span>
+            </Popup>
+          </Marker>
+         )}
     </Map>
   </div>
 </div>
